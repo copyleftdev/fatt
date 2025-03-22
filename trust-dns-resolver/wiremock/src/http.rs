334de8f59c6b729275b00fae2b8c621abfca0997
L337[bf49944f]SIGMA <don@codetestcode.io>
2025-03-22 13:58:55 -0700

    Fix/remove redundant deps (#3)
    * ci: Simplify GitHub Actions workflow and add README badges
    
    * Remove redundant trust-dns-resolver directory
    
    ---------
    
    Co-authored-by: Don Johnson <donj@zuub.com>

--- trust-dns-resolver/wiremock/src/http.rs
+++
@@ -1,3 +0,0 @@
-//! Convenient re-exports of `http-types`' types that are part of `wiremock`'s public API.
-pub use http_types::headers::{HeaderName, HeaderValue, HeaderValues};
-pub use http_types::{Method, Url};
